e08333ff7064f0be67e66243ead442b6c69c5336
apexanalytics <sean@macanalytics.ie>
2022-09-06 16:48:31 +0100

    Changed flick_adhoc and added sample_file.csv

--- R/flick_adhoc_csv.r
+++ R/flick_adhoc_csv.r
@@ -5,17 +5,22 @@
 #
 # The table name assigned is the file name.
 #
+#path <- 'C:\\Users\\SeanMcNally\\OneDrive - MacAnalytics Limited\\Documents\\Workspaces\\flickR\\sample_file.csv'
+
+overwrite = FALSE
+append = FALSE
 
 flick_adhoc_csv  <- function(path, overwrite, append, odbc) {
-odbc <- 'localhost'
 df <- read.csv(path)
 file_name_path <- strsplit(path, split = '\\', fixed = TRUE)
 file_name <- tail(file_name_path[[1]], 1)
 file_name <- gsub(".csv", "", file_name)
 df$file_name <- file_name
+odbc <- 'localhost'
 con <- dbConnect(odbc(), odbc)
 options(scipen=999)
 dbWriteTable(con, Id(schema = "stage", table = file_name), df, overwrite = overwrite, append = append)
-rows <- dbGetQuery(con, statement = paste0("select * from ", file_name, '')) ## want to query the number of records that were loaded and to what table / DB / Server
-msg  <- paste0('File Loaded Successfully. ', rows, ' rows were loaded to the ', file_name, ' table to the ', database, ' database on the ', server, ' server.')
- }
+rows <- dbGetQuery(con, statement = paste0("select count(*) from stage.", file_name, '')) ## want to query the number of records that were loaded and to what table / DB / Server
+confmsg  <- paste0('File Loaded Successfully. ', rows, ' rows were loaded to the stage.', file_name, ' using the ', odbc, ' ODBC connector')
+print(confmsg)
+}
