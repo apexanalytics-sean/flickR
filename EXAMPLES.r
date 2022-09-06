33996977a0fc8a8d0c9ac85f24dff6450b2b5c68
apexanalytics <sean@macanalytics.ie>
2022-09-06 17:56:04 +0100

    Added example for install package from github using devtools and running first function example

--- EXAMPLES.r
+++ EXAMPLES.r
@@ -1,2 +1,4 @@
-# load the sample_file.csv to a SQL table in the localhost DB, overwriting the previous table version
+# load the sample_file.csv to a SQL table in the localhost DB using the localhost ODBC connector, overwriting the previous table version
+library(devtools)
+install_github("apexanalytics/flickR")
 flick_adhoc_csv(path = '.\\sample_file.csv', odbc = 'localhost', overwrite = TRUE, append = FALSE)
