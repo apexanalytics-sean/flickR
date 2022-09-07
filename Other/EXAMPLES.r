3bdf91201f296b44bfc5b5fd929565e8e3ba5d88
apexanalytics <sean@macanalytics.ie>
2022-09-07 18:39:37 +0100

    Added example for flick_csv_folder

--- Other/EXAMPLES.r
+++ Other/EXAMPLES.r
@@ -1,5 +1,12 @@
 # load the sample_file.csv to a SQL table in the localhost DB using the localhost ODBC connector, overwriting the previous table version
+#rm(list=ls())
+
+#folder <- '.\\Other\\sample_csv'
+#odbc <- 'localhost'
+#tablename <- 'sample_file'
+#schema <- 'stage'
 
 #library(devtools)
 #install_github("apexanalytics/flickR")
-#flick_adhoc_csv(file_path = '.\\Other\\sample_file.csv', odbc = 'localhost', overwrite = TRUE, append = FALSE, schema = 'stage')
+#flick_csv(file_path = '.\\Other\\sample_file.csv', odbc = 'localhost', tablename = 'sample_file', schema = 'stage', overwrite = TRUE, append = FALSE)
+#flick_csv_folder(folder = '.\\Other\\sample_csv\\', odbc = 'localhost', tablename = 'multiple_sample_file', schema = 'stage')
