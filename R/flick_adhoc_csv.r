f44ebce51751db57df66054597af947e5a99b3ae
apexanalytics <sean@macanalytics.ie>
2022-09-07 14:41:16 +0100

    updated this to use roxygenise to resolve devtools::document() issue (changed # to #')

--- R/flick_adhoc_csv.r
+++ R/flick_adhoc_csv.r
@@ -1,25 +1,27 @@
-# flick_adhoc_csv
-
-# Load any single CSV file to a SQL table in the stage schema of the
-# database / server specified in the ODBC connector.
-# The table name assigned is the file name.
-# An ODBC connector must be setup, and a schema called 'stage' must
-# be created in the destination database
+#' flick_adhoc_csv!
+#'
+#' Load any single CSV file to a SQL table in the stage schema of the
+#' database / server specified in the ODBC connector.
+#'
+#' The table name assigned is the file name.
+#' An ODBC connector must be setup, and a schema called 'stage' must
+#' be created in the destination database
 #' @export
-#' @param path Path to the file you want to load
+#' @import DBI
+#' @import odbc
+#' @import utils
+#' @param file_path Path to the file you want to load
 #' @param odbc ODBC connector containing destination database details
+#' @param schema Schema in the destination database that you want to load the data to
 #' @param overwrite Overwrite existing table
 #' @param append Append data to table
-#' @param write_to_default_schema Write data to default schema instead of stage schema
 
 flick_adhoc_csv  <- function(file_path, odbc, schema, overwrite, append) {
-  schema <<- schema
   df <- read.csv(file_path)
   file_name_path <- strsplit(file_path, split = '\\', fixed = TRUE)
   file_name <- tail(file_name_path[[1]], 1)
   file_name <- gsub(".csv", "", file_name)
   df$file_name <- file_name
-  #odbc <- 'localhost'
   con <- dbConnect(odbc(), odbc)
   options(scipen=999)
     {dbWriteTable(con, Id(schema = schema, table = file_name), df, overwrite = overwrite, append = append)}
