5c0a24eeecb66ad4a562cdc4b2e1b597456e6058
apexanalytics <sean@macanalytics.ie>
2022-09-07 19:30:11 +0100

    changed tablename to take from parameter

--- R/flick_csv_folder.R
+++ R/flick_csv_folder.R
@@ -20,7 +20,7 @@
 flick_csv_folder  <- function(folder, odbc, tablename, schema) {
   files_in_folder <- as.data.frame(list.files(folder))
   path1 <- paste0(folder,'\\',files_in_folder[1,])
-  flick_csv(file_path = path1, odbc = 'localhost', tablename = 'sample_file', overwrite = TRUE, append = FALSE, schema = schema)
+  flick_csv(file_path = path1, odbc = 'localhost', tablename = tablename, overwrite = TRUE, append = FALSE, schema = schema)
   files_in_folder <- files_in_folder[-1,]
   files_in_folder <- as.data.frame(files_in_folder)
   files_in_folder$folder <- folder
@@ -28,7 +28,7 @@ flick_csv_folder  <- function(folder, odbc, tablename, schema) {
   files_to_load <- as.data.frame(files_in_folder$path)
   for (i in 1:nrow(files_to_load)) {
     print('File Started')
-    flick_csv(file_path = files_to_load[i,], odbc = 'localhost', tablename = 'sample_file', schema = schema, overwrite = FALSE, append = TRUE)
+    flick_csv(file_path = files_to_load[i,], odbc = 'localhost', tablename = tablename, schema = schema, overwrite = FALSE, append = TRUE)
     #paste0('File Load Done. ', files_to_load[i,], ' loaded successfully')
      }
 }
